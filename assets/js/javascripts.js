0ea0aee78592a4cb1827524ad81043ef828931a7
saeed <saaed.amini@gmail.com>
2025-01-25 14:31:51 +0330

    add the first section card

--- assets/js/javascripts.js
+++ assets/js/javascripts.js
@@ -29,6 +29,32 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 //animation gsap
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+let container = document.querySelector("body");
+
+let height;
+function setHeight() {
+  height = container.clientHeight;
+  document.body.style.height = height + "px";
+}
+ScrollTrigger.addEventListener("refreshInit", setHeight);
+
+// smooth scrolling container
+gsap.to(container, {
+  y: () => -(height - document.documentElement.clientHeight),
+  ease: "none",
+  scrollTrigger: {
+    trigger: document.body,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: 1,
+    invalidateOnRefresh: true
+  }
+});
+
+
 const CircleImage = document.querySelectorAll('.circleimg');
 if (typeof gsap !== 'undefined') {
   CircleImage.forEach((img) => {
@@ -44,11 +70,64 @@ if (typeof gsap !== 'undefined') {
   });
 }
 
+let ResponsiveSectionOne = gsap.matchMedia();
+
+ResponsiveSectionOne.add("(min-width: 769px)", () => {
+  const SectionOne = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".First-section",
+      pin: ".main-card-scroll", // پین کردن سکشن کارت‌ها
+      start: "middle middle", // شروع پین از ابتدای سکشن
+      end: "bottom bottom", // مدت زمان پین (به درصد ارتفاع سکشن)
+      scrub: 1.5, // حرکت نرم با اسکرول
+      stagger: 0.5, 
+      markers: true, // نمایش مارکر برای تست
+    },
+    defaults: {
+      duration: 1,
+      ease: "power1.out",
+    },
+  });
+  // انیمیشن برای کارت اول
+  SectionOne
+  // .to(".main-card-scroll" , {
+  //   paddingTop:200,
+  // })
+  .to(".card:nth-child(1)", {
+    x: 0, // جابجایی در محور X
+    y:0,
+    margin:5,
+    scale: 1, // تغییر اندازه
+  })
+
+  // انیمیشن برای کارت دوم
+.to(".card:nth-child(2)", {
+    x: 0,
+    y:0,
+    margin:5,
+    scale: 1, // تغییر اندازه    
+  })
+
+  // انیمیشن برای کارت چهارم (در مرکز قرار بگیرد)
+  .to(".card:nth-child(4)", {
+    x: 0,
+    y:0,
+    margin:5,
+    scale: 1, // تغییر اندازه 
+     })
+   // انیمیشن برای کارت پنجم (در مرکز قرار بگیرد)
+   .to(".card:nth-child(5)", {
+    x: 0,
+    y:0,
+    margin:5,
+    scale: 1, // تغییر اندازه 
+    });
+});
 
 
 
 // saerch button
-  $('.search').click(function () {
+$('.search').click(function () {
     $('.search-box').css('width', '100%');
     $('.search-box').css('opacity', '1');
     $('.search-box input').focus();
