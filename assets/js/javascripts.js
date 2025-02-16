7d3169deaff05ac204db1b107141fb35c074d216
saeed <saaed.amini@gmail.com>
2025-02-16 14:16:19 +0330

    add the faq and the style of that

--- assets/js/javascripts.js
+++ assets/js/javascripts.js
@@ -378,6 +378,38 @@ ResponsiveSectionFive.add("(min-width : 769px)", () => {
 
 // });
 
+// faq
+// Select all question buttons
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+// Loop through each question button
+faqQuestions.forEach(question => {
+  // Add a click event listener to each question
+  question.addEventListener('click', () => {
+    // Close any other open answers except the one clicked
+    faqQuestions.forEach(item => {
+      if (item !== question) {
+        item.classList.remove('active'); // Remove 'active' class to reset arrow rotation
+        item.nextElementSibling.style.maxHeight = null; // Collapse the answer
+      }
+    });
+
+    // Toggle 'active' class on the clicked question to rotate the arrow
+    question.classList.toggle('active');
+
+    // Select the corresponding answer div
+    const answer = question.nextElementSibling;
+
+    // Check if the answer is already open
+    if (answer.style.maxHeight) {
+      // If open, close it by resetting max-height
+      answer.style.maxHeight = null;
+    } else {
+      // If closed, set max-height to scrollHeight to expand it
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    }
+  });
+});
 
 // saerch button
 $('.search').click(function () {
